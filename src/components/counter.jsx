826c96feb687938a8499afd3f36a2fde9533bdb8
Range Quest <range.quest@gmail.com>
2021-09-13 16:46:35 +0530

    conditionally rendered the list.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,34 +4,24 @@ class Counter extends React.Component {
   state = {
     count: 1,
     tags: ['tag1', 'tag2', 'tag3'],
+    // tags: [],
   }
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button type="button" className="btn btn-secondary btn-sm">
-          Increment
-        </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length !== 0 && <p>List not empty</p>}
+        {this.state.tags.length === 0 ? (
+          <p>No Tags</p>
+        ) : (
+          <ul>
+            {this.state.tags.map(tag => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        )}
       </React.Fragment>
     )
-  }
-
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-'
-    classes += this.state.count === 0 ? 'warning' : 'primary'
-    return classes
-  }
-
-  formatCount() {
-    const { count } = this.state
-    const z = 'zero'
-    return count === 0 ? z : count
   }
 }
 
