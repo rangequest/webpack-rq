67cdf191854e27425ff86759310e6a4bcbc66c75
Range Quest <range.quest@gmail.com>
2021-09-13 15:49:52 +0530

    Completed rendering lists.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,30 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 class Counter extends React.Component {
   state = {
-    count: 0,
-    imgUrl: 'https://picsum.photos/200',
-  }
-
-  styles = {
-    span: {
-      fontSize: 14,
-    },
-    red: {
-      color: '#333',
-    },
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3'],
   }
 
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles.span} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button type="button" className="btn btn-secondary btn-sm">
-          <span style={{ ...this.styles.red, fontSize: 30 }}>Increment</span>
+          Increment
         </button>
-        {/* <img src={this.state.imgUrl} alt="rq image" /> */}
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     )
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-'
+    classes += this.state.count === 0 ? 'warning' : 'primary'
+    return classes
   }
 
   formatCount() {
